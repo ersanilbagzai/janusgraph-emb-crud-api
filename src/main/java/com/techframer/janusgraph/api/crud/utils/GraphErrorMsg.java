0c700faec9600ab5ea8f9ceb5593950c2be84e64
sanil.bagzai@azilen.com <sanil.bagzai@azilen.com>
2022-10-05 16:15:16 +0530

    Removed unused files

--- src/main/java/com/techframer/janusgraph/api/crud/utils/GraphErrorMsg.java
+++ src/main/java/com/techframer/janusgraph/api/crud/utils/GraphErrorMsg.java
@@ -21,9 +21,6 @@ public class GraphErrorMsg {
 	/** The Constant GRAPH_MARSHALL_EXCEPTION_MESSAGE. */
 	public static final String GRAPH_MARSHALL_EXCEPTION_MESSAGE = "Could not convert vertex java object into graph vertex object";
 
-	/** The Constant VERTEX_CANT_BE_UPDATED_AS_GIVEN_ID_IS_NULL_MESSAGE. */
-	public static final String VERTEX_CANT_BE_UPDATED_AS_GIVEN_ID_IS_NULL_MESSAGE = "Vertex can't be updated as given ID is null";
-
 	/** The Constant SOURCE_OR_DESTINATION_VERTEX_NOT_EXIST_MESSAGE. */
 	public static final String SOURCE_OR_DESTINATION_VERTEX_NOT_EXIST_MESSAGE = "source or destination vertex does not exist";
 
@@ -51,8 +48,6 @@ public class GraphErrorMsg {
 	public static final String SOURCE_VERTEX_IS_NULL = "source vertex is null";
 	
 	public static final String DESTINATION_VERTEX_IS_NULL = "destination vertex is null";
-	
-	public static final String EDGE_LABEL_PROVIDED_IS_EMPTY = "edgeLabels provided is empty";
 	
 	public static final String MESSAGE_GIVEN_VERTEX_DOES_NOT_HAVE_GID = "Given Vertex does not have gId";
 	
